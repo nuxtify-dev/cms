0754a1286bb76a7db0d62d1225785911172a75aa
David Stack <2013388+davidstackio@users.noreply.github.com>
2026-05-31 16:43:07 -0400

    fix: proper type import path

--- src/module.ts
+++ src/module.ts
@@ -9,7 +9,7 @@ import { defu } from 'defu'
 import { name, version } from '../package.json'
 
 // Types
-import type { ModuleOptions } from './types/types'
+import type { ModuleOptions } from './types/module'
 
 export default defineNuxtModule<ModuleOptions>().with({
   meta: {
