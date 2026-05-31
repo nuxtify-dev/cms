616e49727812c71e53308e5381c856cd377eaed0
David Stack <2013388+davidstackio@users.noreply.github.com>
2026-05-31 14:39:38 -0400

    fix(dx): remove redundant FixSitemap type

--- src/types/content.ts
+++ src/types/content.ts
@@ -10,25 +10,20 @@ import type {
 
 export type Image = z.infer<typeof imageSchema>
 
-type FixSitemap<T> = Omit<T, 'sitemap'> & {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  sitemap?: any
-}
-
-export interface Page extends PageCollectionItemBase, Omit<FixSitemap<z.infer<typeof pageSchema>>, 'draft'> {
+export interface Page extends PageCollectionItemBase, Omit<z.infer<typeof pageSchema>, 'draft'> {
   draft?: boolean
 }
 
-export interface Contributor extends PageCollectionItemBase, Omit<FixSitemap<z.infer<typeof contributorSchema>>, 'draft'> {
+export interface Contributor extends PageCollectionItemBase, Omit<z.infer<typeof contributorSchema>, 'draft'> {
   draft?: boolean
 }
 export type ContributorSocials = NonNullable<Contributor['socials']>
 
-export interface Article extends PageCollectionItemBase, Omit<FixSitemap<z.infer<typeof articleSchema>>, 'draft'> {
+export interface Article extends PageCollectionItemBase, Omit<z.infer<typeof articleSchema>, 'draft'> {
   draft?: boolean
 }
 
-export interface Topic extends PageCollectionItemBase, Omit<FixSitemap<z.infer<typeof topicSchema>>, 'draft'> {
+export interface Topic extends PageCollectionItemBase, Omit<z.infer<typeof topicSchema>, 'draft'> {
   draft?: boolean
 }
 
