1e0202803167dfde8ccdb489c426b0f5437b4698
David Stack <2013388+davidstackio@users.noreply.github.com>
2026-08-08 14:29:40 -0400

    playground: optimize vite deps

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
   vite: {
     optimizeDeps: {
       include: [
+        '@unhead/schema-org/vue',
         '@vue/devtools-core',
         '@vue/devtools-kit',
       ],
